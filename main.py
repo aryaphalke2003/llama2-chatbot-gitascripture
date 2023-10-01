926c1a4e64694d0dac3850ae8b9534520e66e55c
aryaphalke2003 <aryaphalke2003@gmail.com>
2023-10-02 00:44:01 +0530

    main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def retrieval_qa_chain(llm, prompt, db):
 def load_llm():
     # Load the locally downloaded model here
     llm = CTransformers(
-        model = "https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGML/blob/main/llama-2-7b-chat.ggmlv3.q8_0.bin",
+        model = "./models/llama-2-7b-chat.ggmlv3.q8_0.bin",
         model_type="llama",
         max_new_tokens = 512,
         temperature = 0.5
